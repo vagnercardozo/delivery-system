048d35ddd91969ab934cff51386c14a38ed297c1
Vagner Cardozo <vagner.cardozo@noctech.io>
2026-01-29 12:55:26 -0300

    feat: Primeiro commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+export default {
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.jsx",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
