a48190c8f6f2a1c0009daf7df96c5bca9a750904
Vagner Cardozo <vagner.cardozo@noctech.io>
2026-02-02 19:11:32 -0300

    feat: Adicionado sanctum

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,12 +8,14 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
+    use HasApiTokens;
+
     /** @use HasFactory<UserFactory> */
     use HasFactory;
-
     use Notifiable;
     use TwoFactorAuthenticatable;
 
