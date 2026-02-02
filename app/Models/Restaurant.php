fc1f3772e10563385c2e2122793b3e6b3dc248b1
Vagner Cardozo <vagner.cardozo@noctech.io>
2026-02-02 13:22:34 -0300

    feat: Adicionada estrategia para gerar slug

--- app/Models/Restaurant.php
+++ app/Models/Restaurant.php
@@ -6,13 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+/**
+ * @property string $name
+ * @property int $id
+ * @property string $slug
+ */
 class Restaurant extends Model
 {
     use SoftDeletes;
 
-    public int $id;
-
-    public string $name;
 
     protected $fillable = [
         'name',
--- app/Observers/RestaurantObserver.php
+++ app/Observers/RestaurantObserver.php
@@ -7,9 +7,18 @@ use Illuminate\Support\Str;
 
 class RestaurantObserver
 {
+    public function creating(Restaurant $restaurant): void
+    {
+        if (!empty($restaurant->slug)) return;
+
+        $restaurant->slug = Str::uuid();
+
+    }
+
     public function created(Restaurant $restaurant): void
     {
         $slug = Str::slug($restaurant->name).'-'.base_convert($restaurant->id, 10, 36);
-        $restaurant->update(['slug' => $slug]);
+        $restaurant->slug = $slug;
+        $restaurant->saveQuietly();
     }
 }
